00764fb85753b5b735b4dc0f204c4ce0a34c2ef0
Lucas Carvalho <lucascarvalhojf@hotmail.com>
2018-04-17 10:58:13 -0300

    Login com facebook

--- app/src/main/java/com/lucas/pickapic/LoginActivity.java
+++ app/src/main/java/com/lucas/pickapic/LoginActivity.java
@@ -5,16 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = findViewById(R.id.login_button);
     }
 
     public void login(View view) {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
+        final Intent intent = new Intent(this, HomeActivity.class);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult result) {
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException fe) {
+
+            }
+        });
+
     }
+
 }
